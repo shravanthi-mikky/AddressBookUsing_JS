2a41f4173db70644caa46ba58e7e9193c358026b
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-06-24 20:12:29 +0530

    [shravanthi] -create addressBook array and add contacts to it

--- addressBook.js
+++ addressBook.js
@@ -23,3 +23,19 @@ class AddressBook{
 
     }
 }
+var contact1=new AddressBook("Shravanthi","Pabboji","Old Alwal","Alwal","Telangana",500010,9000000001,"shravanthi@gmail.com");
+//contact1.printDetails();
+var contact2=new AddressBook("Vishnu","Nali","Old Alwal","Alwal","Telangana",500010,9000000002,"vishnu@gmail.com");
+var contact3=new AddressBook("Roshni","adatrao","Old Alwal","Alwal","Telangana",500010,9000000003,"roshni@gmail.com");
+//contact2.printDetails();
+//contact3.printDetails();
+var map1= new Map();
+map1.set(1,contact1);
+map1.set(2,contact2);
+map1.set(3,contact3);
+//console.log(map1.size);
+var array=[];
+array.push(contact1);
+array.push(contact2);
+array.push(contact3);
+console.log(array[0].firstName);
